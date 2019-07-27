04a04c343e5c17cebb41ac1c727d345c8d497030
meizi8 <q70476547@vip.qq.com>
2019-07-28 03:50:20 +0800

    日期组件基本完成

--- src/components/module/timeModule.vue
+++ src/components/module/timeModule.vue
@@ -6,30 +6,32 @@
 		</div>
 		<div class="body">
 			<div class="control">
-				<div class="selectMouth">{{selectMouth | moment('YYYY年MM月')}}</div>
+				<div class="selectMonth">{{selectMonth | moment('YYYY年MM月')}}</div>
 				<div class="arrow">
-					<div class="arrow-top">
+					<div class="arrow-top" @click="selectLastMonth">
 						<i class="iconfont icon-arrow-up-s"></i>
 					</div>
-					<div class="arrow-bottom">
+					<div class="arrow-bottom" @click="selectNextMonth">
 						<i class="iconfont icon-arrow-down-s"></i>
 					</div>
 				</div>
 			</div>
 			<div class="table">
 				<div class="title">
-					<div class="period" v-for="(item, index) in ['一','二','三','四','五','六','日']" :key="index">
+					<div class="period" v-for="(item, index) in ['一','二','三','四','五','六','日']" :key="'key1-'+index">
 						{{item}}
 					</div>
 				</div>
-				<div class="date" v-if="mouthDays">
-					<div class="day" v-for="(day,idx) in mouthDays" :key="idx">
-						<div v-if="curTime.getDate() == day" class="active">
-							{{day}}
-						</div>
-						<div v-else>
-							{{day}}
-						</div>
+				<div class="date" v-if="monthDays">
+					<div class="day other" v-for="(day,index) in lastMonthDays" :key="'key2-'+index">
+						{{day | moment('D')}}
+					</div>
+					<div class="day" v-for="(day,index) in monthDays" :key="index"
+						:class="curTime.getDate() == day.getDate() && curTime.getFullYear() == day.getFullYear() && curTime.getMonth() == day.getMonth()? 'active':''">
+						{{day | moment('D')}}
+					</div>
+					<div class="day other" v-for="(day,index) in nextMonthDays" :key="'key3-'+index">
+						{{day| moment('D')}}
 					</div>
 				</div>
 			</div>
@@ -41,34 +43,76 @@
 	export default {
 		data() {
 			return {
-				selectMouth: new Date(),
-				firstDay: null,
-				lastDay: null,
-				mouthDays: null,
+				selectMonth: new Date(), //当前选中月份
+				firstDay: null, //选中月份的第一天
+				nextMonthFristDay: null, //选中月份的下个月第一天
+				monthDays: null, //当前月总共天数
+				lastMonthDays: [], //显示上个月的天数
+				nextMonthDays: 0, //显示下个月的天数
+				col: 6, //显示6行
 			}
 		},
 		computed: {
-			curTime() {
+			curTime() { //当前时间
 				return this.$store.state.curTime;
 			}
 		},
 		watch: {
-			selectMouth(value) {
-				console.log(value);
-				this.firstDay = new Date(new Date(this.selectMouth).setDate(1));
-				console.log('firstDay', this.firstDay);
-				this.lastDay = new Date(new Date(this.firstDay).setMonth(this.firstDay.getMonth() + 1));
-				console.log('lastDay', this.lastDay);
-				this.mouthDays = (this.lastDay - this.firstDay) / 86400000;
-				console.log('mouthDays', this.mouthDays);
+			selectMonth(value) {
+				this.firstDay = this.getFirstDay(value);
+				this.nextMonthFristDay = this.getNextMonthFristDay(this.firstDay);
+				this.monthDays = this.getMonthDays(this.nextMonthFristDay);
+				this.lastMonthDays = this.getLastMonthDays(this.firstDay);
+				this.nextMonthDays = this.getNextMonthDays(this.lastMonthDays.length, this.monthDays.length, this
+					.nextMonthFristDay);
 			}
 		},
 		created() {
-			// this.selectMouth = new Date();
-			var date = new Date();
-			date.setMonth(date.getMonth()+1);
-			this.selectMouth = date;
+			this.selectMonth = new Date();
 		},
+		methods: {
+			getFirstDay(month) {
+				return new Date(month.setDate(1));
+			},
+			getNextMonthFristDay(firstDay) {
+				return new Date(new Date(this.firstDay).setMonth(this.firstDay.getMonth() + 1));
+			},
+			getMonthDays(nextMonthFristDay) {
+				const lastDay = new Date(nextMonthFristDay.getTime() - 86400000);
+				let count = 0;
+				const allDate = lastDay.getDate();
+				const curMonthDay = [];
+				while (allDate > count++) {
+					curMonthDay.push(new Date(new Date(lastDay).setDate(count)));
+				}
+				return curMonthDay;
+			},
+			getLastMonthDays(firstDay) {
+				const lastMonthDays = [];
+				for (var i = 1; i < firstDay.getDay(); i++) {
+					const date = new Date(firstDay.getTime() - 86400000 * i);
+					date.id = Math.floor(Math.random() * 10000);
+					lastMonthDays.push(date)
+				}
+				return lastMonthDays;
+			},
+			getNextMonthDays(lastMonthDaysLength, monthDaysLength, nextMonthFristDay) {
+				const nextMonthDays = [];
+				const needShowSum = this.col * 7 - lastMonthDaysLength - monthDaysLength;
+				for (var i = 0; i < needShowSum; i++) {
+					const date = new Date(nextMonthFristDay.getTime() + 86400000 * i);
+					date.id = Math.floor(Math.random() * 10000);
+					nextMonthDays.push(date);
+				}
+				return nextMonthDays;
+			},
+			selectLastMonth() {
+				this.selectMonth = new Date(this.selectMonth.setMonth(this.selectMonth.getMonth() - 1));
+			},
+			selectNextMonth() {
+				this.selectMonth = new Date(this.selectMonth.setMonth(this.selectMonth.getMonth() + 1));
+			},
+		}
 	}
 
 </script>
@@ -104,7 +148,7 @@
 			line-height: 50px;
 			padding: 0 20px;
 
-			.selectMouth {
+			.selectMonth {
 				flex: 5;
 				font-size: 14px;
 			}
@@ -146,13 +190,12 @@
 				}
 
 				.day {
-					.active {
-						width: 100%;
-						height: 100%;
-						display: flex;
-						align-items: center;
-						justify-content: center;
+					&.active {
 						background-color: #7a7574;
+					}
+
+					&.other {
+						color: #777;
 					}
 
 					&:hover {
