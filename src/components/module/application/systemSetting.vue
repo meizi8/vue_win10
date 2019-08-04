79c26d7b8b2837da3364f56f00f89afbd41617fc
meizi8 <q70476547@vip.qq.com>
2019-08-04 22:58:28 +0800

    添加自定义指令，实现应用程序拖拽

--- src/components/module/application/systemSetting.vue
+++ src/components/module/application/systemSetting.vue
@@ -1,30 +1,35 @@
 <template>
-	<div id="systemSetting" style="width:1022px;height:730px">
-		<div class="setting-left">
+	<div id="systemSetting" style="width:1022px;height:730px" ref="move">
+		<div class="win-header" v-drag:target="this.$refs">
 			<div class="win-title">设置</div>
-			<div class="subject">
-				<div class="title">个性化</div>
-				<ul class="menu">
-					<li v-for="item in menuList" :key="item.flot" :class="page==item.flot?'active':''" @click='togglePage(item.flot)'>
-						<i class="icon"></i>
-						<span>{{item.text}}</span>
-					</li>
-				</ul>
-			</div>
-
-		</div>
-		<div class="setting-rigth">
 			<div class="win-control">
 				<div class="minimize">最小化</div>
 				<div class="Windowed">窗口化</div>
 				<div class="maximize">最大化</div>
 				<div class="close" @click="close">关闭</div>
 			</div>
-			<div class="backgrounp" v-show="page=='backgrounp'">
-				背景页面
+		</div>
+		<div class="win-content">
+			<div class="setting-left">
+				<div class="subject">
+					<div class="title">个性化</div>
+					<ul class="menu">
+						<li v-for="item in menuList" :key="item.flot" :class="page==item.flot?'active':''"
+							@click='togglePage(item.flot)'>
+							<i class="icon"></i>
+							<span>{{item.text}}</span>
+						</li>
+					</ul>
+				</div>
+
 			</div>
-			<div class="color" v-show="page=='color'">
-				颜色页面
+			<div class="setting-rigth">
+				<div class="backgrounp" v-show="page=='backgrounp'">
+					背景页面
+				</div>
+				<div class="color" v-show="page=='color'">
+					颜色页面
+				</div>
 			</div>
 		</div>
 	</div>
@@ -35,12 +40,12 @@
 			return {
 				page: '',
 				menuList: [{
-					flot:'backgrounp',
-					text:'背景',
-				},{
-					flot:'color',
-					text:'颜色',
-				},]
+					flot: 'backgrounp',
+					text: '背景',
+				}, {
+					flot: 'color',
+					text: '颜色',
+				}, ]
 			}
 		},
 		created() {
@@ -50,11 +55,11 @@
 			})
 		},
 		methods: {
-			togglePage(e){
+			togglePage(e) {
 				this.page = e;
 			},
 
-			close(){
+			close() {
 				this.bus.$emit('destroy')
 			}
 		},
@@ -67,65 +72,88 @@
 		position: absolute;
 		left: 20px;
 		top: 50px;
-		display: flex;
 		border: 1px solid #707070;
 
-		.setting-left {
-			width: 320px;
-			background-color: rgba(255, 255, 255, .9);
+		.win-header {
+			position: absolute;
+			height: 35px;
+			line-height: 35px;
+			width: 100%;
 
-			.subject {
-				.menu {
-					line-height: 48px;
-					font-size: 14px;
+			.win-title {
+				float: left;
+			}
+			.win-control {
+				float: right;
+				display: flex;
+			}
+		}
 
-					li {
-						position: relative;
-						cursor: pointer;
+		.win-content {
+			display: flex;
+			height: 100%;
 
-						span {
-							padding-left: 50px;
-						}
+			.setting-left {
+				padding-top: 35px;
+				width: 320px;
+				background-color: rgba(255, 255, 255, .9);
 
-						.icon {
-							position: absolute;
-							left: 18px;
-							top: 50%;
-							transform: translateY(-50%);
-							background-color: yellow;
-							height: 20px;
-							width: 20px;
-						}
+				.subject {
+					.title {
+						height: 30px;
+						line-height: 20px;
+						font-weight: 600;
+					}
+					.menu {
+						line-height: 48px;
+						font-size: 14px;
 
-						&:hover {
-							background-color: rgba(200, 200, 200, .9);
-						}
+						li {
+							position: relative;
+							cursor: pointer;
 
-						&.active::after {
-							content: '';
-							position: absolute;
-							left: 0;
-							top: 50%;
-							transform: translateY(-50%);
-							width: 5px;
-							height: 25px;
-							background-color: #7a7574;
+							span {
+								padding-left: 50px;
+							}
+
+							.icon {
+								position: absolute;
+								left: 18px;
+								top: 50%;
+								transform: translateY(-50%);
+								background-color: yellow;
+								height: 20px;
+								width: 20px;
+							}
+
+							&:hover {
+								background-color: rgba(200, 200, 200, .9);
+							}
+
+							&.active::after {
+								content: '';
+								position: absolute;
+								left: 0;
+								top: 50%;
+								transform: translateY(-50%);
+								width: 5px;
+								height: 25px;
+								background-color: #7a7574;
+							}
 						}
 					}
 				}
+
 			}
 
-		}
-
-		.setting-rigth {
-			flex: 1;
-			background-color: #fff;
-
-			.win-control {
-				display: flex;
-				justify-content: flex-end;
+			.setting-rigth {
+				padding-top: 35px;
+				flex: 1;
+				background-color: #fff;
 			}
 		}
+
+
 	}
 
 </style>
--- src/main.js
+++ src/main.js
@@ -2,19 +2,42 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store'
-
-Vue.use(require('vue-moment'));
-
 import './base.css'
 import './assets/iconfont/iconfont.css'
+
+Vue.use(require('vue-moment'));
+Vue.directive('drag', {
+	inserted: function (el,binding,vnode) {
+		const target = binding.value.move;
+		el.onmousedown = (e) => {
+			//算出鼠标相对元素的位置
+			let disX = e.clientX - target.offsetLeft;
+			let disY = e.clientY - target.offsetTop;
+
+			document.onmousemove = (e)=>{
+				//用鼠标的位置减去鼠标相对元素的位置，得到元素的位置
+				let left = e.clientX - disX;
+				let top = e.clientY - disY;
+
+				//移动当前元素
+				target.style.left = left + 'px';
+				target.style.top = top + 'px';
+			};
+			document.onmouseup = (e) => {
+				document.onmousemove = null;
+				document.onmouseup = null;
+			};
+		};
+	}
+})
 
 Vue.config.devtools = true;
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  store,
-  router,
-  render: h => h(App)
+	el: '#app',
+	store,
+	router,
+	render: h => h(App)
 })
