57e8db35e32561e06c7257d829f20ba258c1fa48
meizi8 <q70476547@vip.qq.com>
2019-08-10 03:10:06 +0800

    完成应用缩放

--- src/components/module/application/common/window-contarl.vue
+++ src/components/module/application/common/window-contarl.vue
@@ -2,14 +2,14 @@
 	<div style="width:1022px;height:730px;left: 20px;top: 50px;" class="window-contorl" @click="contorlClick"
 		@contextmenu.prevent.stop="contorlClick" ref="move">
 		<div class="border-cursor">
-			<div class="right type1" v-right></div>
-			<div class="left type1" v-left></div>
-			<div class="top type1" v-top></div>
-			<div class="bottom type1" v-bottom></div>
-			<div class="right-top type2"></div>
-			<div class="left-top type2"></div>
-			<div class="right-bottom type2"></div>
-			<div class="left-bottom type2"></div>
+			<div class="right type1" v-scale.right></div>
+			<div class="left type1" v-scale.left></div>
+			<div class="top type1" v-scale.top></div>
+			<div class="bottom type1" v-scale.bottom></div>
+			<div class="right-top type2" v-scale.right.top></div>
+			<div class="left-top type2" v-scale.left.top></div>
+			<div class="right-bottom type2" v-scale.right.bottom></div>
+			<div class="left-bottom type2" v-scale.left.bottom></div>
 		</div>
 		<div class="content">
 			<slot v-if="fatherDom" :father="fatherDom"></slot>
@@ -38,116 +38,85 @@
 			}
 		},
 		directives: {
-			left: {
+			scale: {
 				update: function (el, binding, vnode) {
 					const windowContorl = vnode.context.$el;
 					const desktop = windowContorl.parentNode;
 					const desktopClientWidth = desktop.clientWidth;
+					const desktopClientHeight = desktop.clientHeight;
+					const minHeight = parseInt(windowContorl.style.minHeight) || 0;
 					const minWidth = parseInt(windowContorl.style.minWidth) || 0;
 
 					el.onmousedown = e => {
-						const windowContorlWdith = parseInt(windowContorl.style.width);
-						const lastPosition = windowContorl.style.right == '';
-						const left = parseInt(windowContorl.style.left);
-
-						windowContorl.style.left = '';
-						windowContorl.style.right = (lastPosition ?  desktopClientWidth - left - windowContorlWdith : parseInt(windowContorl.style.right))+'px';
-
-						const disX = e.clientX;
-
-						document.onmousemove = e => {
-							const x = e.clientX - disX;
-							if (windowContorlWdith + x < minWidth) return;
-							windowContorl.style.width = windowContorlWdith - x + 'px';
-						};
-						document.onmouseup = e => {
-							document.onmousemove = null;
-							document.onmouseup = null;
-						};
-					}
-				}
-			},
-			right: {
-				update: function (el, binding, vnode) {
-					const windowContorl = vnode.context.$el;
-					const desktop = windowContorl.parentNode;
-					const desktopClientWidth = desktop.clientWidth;
-					const minWidth = parseInt(windowContorl.style.minWidth) || 0;
-
-					el.onmousedown = e => {
-						const windowContorlWdith = parseInt(windowContorl.style.width);
-						const lastPosition = windowContorl.style.left == '';
-						const right = parseInt(windowContorl.style.right);
-
-						windowContorl.style.right = '';
-						windowContorl.style.left = (lastPosition ?  desktopClientWidth - right - windowContorlWdith : parseInt(windowContorl.style.left))+'px';
-
-						const disX = e.clientX;
-
-						document.onmousemove = e => {
-							const x = e.clientX - disX;
-							if (windowContorlWdith + x < minWidth) return;
-							windowContorl.style.width = windowContorlWdith + x + 'px';
-						};
-						document.onmouseup = e => {
-							document.onmousemove = null;
-							document.onmouseup = null;
-						};
-					}
-				}
-			},
-			bottom: {
-				update: function (el, binding, vnode) {
-					const windowContorl = vnode.context.$el;
-					const desktop = windowContorl.parentNode;
-					const desktopClientHeight = desktop.clientHeight;
-					const minHeight = parseInt(windowContorl.style.minHeight) || 0;
-
-					el.onmousedown = e => {
 						const windowContorlHeight = parseInt(windowContorl.style.height);
-						const lastPosition = windowContorl.style.top == '';
-						const bottom = parseInt(windowContorl.style.bottom);
+						const windowContorlWdith = parseInt(windowContorl.style.width);
 
-						windowContorl.style.bottom = '';
-						windowContorl.style.top = (lastPosition ?  desktopClientHeight - bottom - windowContorlHeight : parseInt(windowContorl.style.top))+'px';
+						let top, right, bottom, left, disX, disY;
 
-						const disY = e.clientY;
+						if(binding.modifiers.top){
+							top = parseInt(windowContorl.style.top);
+
+							windowContorl.style.top = '';
+							windowContorl.style.bottom = desktopClientHeight - top - windowContorlHeight + 'px';
+
+							disY = e.clientY;
+						}
+
+						if(binding.modifiers.bottom){
+							bottom = parseInt(windowContorl.style.bottom);
+
+							windowContorl.style.top = desktopClientHeight - bottom - windowContorlHeight + 'px';
+
+							disY = e.clientY;
+						}
+
+						if(binding.modifiers.left){
+							left = parseInt(windowContorl.style.left);
+
+							windowContorl.style.left = '';
+							windowContorl.style.right = desktopClientWidth - left - windowContorlWdith + 'px';
+
+							disX = e.clientX;
+						}
+
+						if(binding.modifiers.right){
+							right = parseInt(windowContorl.style.right);
+
+							windowContorl.style.left = desktopClientWidth - right - windowContorlWdith + 'px';
+
+							disX = e.clientX;
+						}
 
 						document.onmousemove = e => {
-							const y = e.clientY - disY;
-							if (windowContorlHeight + y < minHeight) return;
-							windowContorl.style.height = windowContorlHeight + y + 'px';
+							if(binding.modifiers.top || binding.modifiers.bottom){
+								const y = e.clientY - disY;
+								if (windowContorlHeight + (binding.modifiers.top?-y:y) < minHeight) {
+									windowContorl.style.height = minHeight + 'px';
+								} else {
+									windowContorl.style.height = windowContorlHeight +(binding.modifiers.top?-y:y) + 'px';
+								}
+							}
+
+							if(binding.modifiers.left || binding.modifiers.right){
+								const x = e.clientX - disX;
+								if (windowContorlWdith + (binding.modifiers.left?-x:x) < minWidth) {
+									windowContorl.style.width = minWidth + 'px';
+								} else {
+									windowContorl.style.width = windowContorlWdith + (binding.modifiers.left?-x:x) + 'px';
+								}
+							}
 						};
 						document.onmouseup = e => {
-							document.onmousemove = null;
-							document.onmouseup = null;
-						};
-					}
-				}
-			},
-			top: {
-				update: function (el, binding, vnode) {
-					const windowContorl = vnode.context.$el;
-					const desktop = windowContorl.parentNode;
-					const desktopClientHeight = desktop.clientHeight;
-					const minHeight = parseInt(windowContorl.style.minHeight) || 0;
+							if(binding.modifiers.left){
+								windowContorl.style.right = '';
+								windowContorl.style.left = e.clientX + 'px';
+							}
 
-					el.onmousedown = e => {
-						const windowContorlHeight = parseInt(windowContorl.style.height);
-						const lastPosition = windowContorl.style.bottom == '';
-						const top = parseInt(windowContorl.style.top);
+							if(binding.modifiers.top){
+								windowContorl.style.bottom = '';
+								windowContorl.style.top = e.clientY + 'px';
+							}
 
-						windowContorl.style.top = '';
-						windowContorl.style.bottom = (lastPosition ?  desktopClientHeight - top - windowContorlHeight : parseInt(windowContorl.style.bottom))+'px';
-
-						const disY = e.clientY;
-
-						document.onmousemove = e => {
-							const y = e.clientY - disY;
-							if (windowContorlHeight + y < minHeight) return;
-							windowContorl.style.height = windowContorlHeight - y + 'px';
-						};
-						document.onmouseup = e => {
 							document.onmousemove = null;
 							document.onmouseup = null;
 						};
